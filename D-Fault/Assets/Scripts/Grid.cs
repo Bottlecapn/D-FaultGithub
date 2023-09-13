33c72816283ea03ea704337182661462f06c9e54
Bottlecapn <bottlecapn@gmail.com>
2023-09-12 19:53:16 -0700

    Grid basics

--- D-Fault/Assets/Scripts/Grid.cs
+++ D-Fault/Assets/Scripts/Grid.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+    [SerializeField] int gridSizeX, gridSizeY;
+    [SerializeField] GameObject basicTile;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateGrid();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void GenerateGrid()
+    {
+        for (int y = 0; y < gridSizeY; y++)
+        {
+            for (int x = 0; x < gridSizeX; x++)
+            {
+                GameObject go = Instantiate(basicTile, transform);
+                go.transform.position = new Vector3(transform.position.x + x, transform.position.y, 
+                    transform.position.z + y);
+                Tile t = go.GetComponent<Tile>();
+                t.SetPosition(x,y);
+            }
+        }
     }
 }
---
+++ D-Fault/Assets/Scripts/Tile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private int posX;
+    private int posY;
+
+    public void SetPosition(int x, int y)
+    {
+        posX = x;
+        posY = y;
+    }
+
+    public int[] GetPosition()
+    {
+        int[] arr = {posX, posY};
+        return arr;
+    }
+}
