96798f6be18c3c703f2103193231eaed777db397
Bottlecapn <bottlecapn@gmail.com>
2023-09-13 00:43:32 -0700

    Grid script changes

--- D-Fault/Assets/Scripts/TileGrid.cs
+++ D-Fault/Assets/Scripts/TileGrid.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid : MonoBehaviour
+public class TileGrid : MonoBehaviour
 {
     [SerializeField] int gridSizeX, gridSizeY;
     [SerializeField] GameObject basicTile;
+    [SerializeField] List<Tile> tiles;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +30,7 @@ public class Grid : MonoBehaviour
                     transform.position.z + y);
                 Tile t = go.GetComponent<Tile>();
                 t.SetPosition(x,y);
+                tiles.Add(t);
             }
         }
     }
