390005019e92a47d6f4fbfaed311c3c0281c4fbf
Bottlecapn <bottlecapn@gmail.com>
2023-09-13 18:15:44 -0700

    Update Die.cs

--- D-Fault/Assets/Scripts/Die.cs
+++ D-Fault/Assets/Scripts/Die.cs
@@ -1,68 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class Die : MonoBehaviour
 {
     public int Moves;
+    //public TextMeshPro moveNumText;
     public int GridSize;
-    private bool mLastFrameUp = false;
-    private bool mLastFrameDown = false;
-    private bool mLastFrameLeft = false;
-    private bool mLastFrameRight = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(GridSize / 2, 0.5f, GridSize / 2); //0.5f should be half the size in z direction
+        transform.position = new Vector3(GridSize / 2, 0.5f, GridSize / 2); //0.5f should be half the die size in z direction
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if there are no moves left for the current die, destroy it
-        if (Moves <= 0)
+        if (Moves > 0)
         {
-            Destroy(this.gameObject);
+            float verticalMove = 0.0f;
+            float horizontalMove = 0.0f;
+            // vertical movement
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            {
+                // boundary check
+                if (transform.position.z + 1 < GridSize)
+                {
+                    Moves--;
+                    verticalMove = 1.0f;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            {
+                // boundary check
+                if (transform.position.z + 1 > 1)
+                {
+                    Moves--;
+                    verticalMove = -1.0f;
+                }
+            }
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + verticalMove);
+            // horizontal movement
+            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            {
+                // boundary check
+                if (transform.position.x - 1 > 1)
+                {
+                    Moves--;
+                    horizontalMove = -1.0f;
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            {
+                // boundary check
+                if (transform.position.x + 1 < GridSize)
+                {
+                    Moves--;
+                    horizontalMove = 1.0f;
+                }
+            }
+            transform.position = new Vector3(transform.position.x + horizontalMove, transform.position.y, transform.position.z);
         }
-
-        // move on leading edge
-        int verticalMove = 0;
-        int horizontalMove = 0;
-        if (!mLastFrameUp && (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)))
-        {
-            Moves--;
-            verticalMove++;
-        }
-        if (!mLastFrameDown && (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)))
-        {
-            Moves--;
-            verticalMove--;
-        }
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + verticalMove);
-
-        if (!mLastFrameLeft && (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)))
-        {
-            Moves--;
-            horizontalMove--;
-        }
-        if (!mLastFrameRight && (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)))
-        {
-            Moves--;
-            horizontalMove++;
-        }
-        transform.position = new Vector3(transform.position.x + horizontalMove, transform.position.y, transform.position.z);
-
-        mLastFrameUp = (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W));
-        mLastFrameDown = (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S));
-        mLastFrameLeft = (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A));
-        mLastFrameRight = (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D));
 
         // Show numbers on the die
         // NOTE - Amari: I can do this, yes
-
-        // border check
 
         // Add system (if collide with another die, destroy the other and add its moves to current one)
         // NOTE - Amari: I think instead of destroying dice, the other die simply stops at it's last position. 
