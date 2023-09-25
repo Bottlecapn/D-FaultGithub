95b8b37325abc538e95aa28a32e8991313dc27c0
Krishna Biniwale <kbiniwal@usc.edu>
2023-09-24 21:48:00 -0700

    Added level select and changed design

--- D-Fault/Assets/Scripts/MainMenu.cs
+++ D-Fault/Assets/Scripts/MainMenu.cs
@@ -5,35 +5,132 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    static string sceneName;
+    float startTime = 0;
+
+    private void ChangeScene() {
+        SceneManager.LoadScene(sceneName);
+        sceneName = "";
+    }
 
     public void PlayGame()
     {
-        Invoke("PlayGameInvoke", 2);
-    }
-    private void PlayGameInvoke()
-    {
-        SceneManager.LoadScene("AnimTesting");
+        sceneName = "AnimTesting";
+        Invoke("ChangeScene", 1);
     }
 
     public void LevelSelect()
     {
-        Invoke("LevelSelectInvoke", 1);
-    }
-
-    private void LevelSelectInvoke()
-    {
-        SceneManager.LoadScene("LevelSelect");
+        sceneName = "LevelSelect";
+        Invoke("ChangeScene", 1);
     }
 
     public void ReturnMainMenu()
     {
-        Invoke("MainMenuInvoke", 1);
-    }
-    public void MainMenuInvoke()
-    {
-        SceneManager.LoadScene("MainMenu");
+        sceneName = "MainMenu";
+        Invoke("ChangeScene", 1);
     }
 
+    public void Level1() {
+        sceneName = "Level1";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level2() {
+        sceneName = "Level2";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level3() {
+        sceneName = "Level3";
+        Invoke("ChangeScene", 1);
+    }
+/*
+    public void Level4() {
+        sceneName = "Level4";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level5() {
+        sceneName = "Level5";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level6() {
+        sceneName = "Level6";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level7() {
+        sceneName = "Level7";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level8() {
+        sceneName = "Level8";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level9() {
+        sceneName = "Level9";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level10() {
+        sceneName = "Level10";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level11() {
+        sceneName = "Level11";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level12() {
+        sceneName = "Level12";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level13() {
+        sceneName = "Level13";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level14() {
+        sceneName = "Level14";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level15() {
+        sceneName = "Level15";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level16() {
+        sceneName = "Level16";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level17() {
+        sceneName = "Level17";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level18() {
+        sceneName = "Level18";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level19() {
+        sceneName = "Level19";
+        Invoke("ChangeScene", 1);
+    }
+
+    public void Level20() {
+        sceneName = "Level20";
+        Invoke("ChangeScene", 1);
+    }
+*/
     public void QuitGame()
     {
         Application.Quit();
